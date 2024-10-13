1e747b8b318641734b8cec5e78c62aa6b877b35c
shubhramani21 <ramanishubh@gmail.com>
2024-10-14 01:02:59 +0530

    working with linked list

--- 10. Linked List/LearningPart/intro.cpp
+++ 10. Linked List/LearningPart/intro.cpp
@@ -1,42 +1,121 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+template <typename T>
 class Node {
 public:
-    int data;
+    T data;
     static int nodeCount;  
     Node* next;
 
 public:
-    Node(int data, Node* next) : data(data), next(next) {
+    Node(T data, Node* next) : data(data), next(next) {
         nodeCount++;
     }
 
-    Node(int data) : data(data), next(nullptr) {
+    Node(T data) : data(data), next(nullptr) {
         nodeCount++;
     }
-    
+
     ~Node() {
         nodeCount--;
     }
 };
 
 // Static member initialization outside the class
-int Node::nodeCount = 0;
+template <typename T>
+int Node<T>::nodeCount = 0;
 
+
+
+Node<int>* createArray2List(vector<int> &arr){
+    int n = arr.size();
+    Node<int>* head = new Node<int>(arr[0]);
+
+    Node<int>* mover = head;
+
+    for (int i = 1; i < n; i++)
+    {
+        Node<int>* temp = new Node<int>(arr[i]);
+        mover->next = temp;
+
+        mover = mover->next;
+    }
+
+    return head;
+}
+
+
+Node<int>* removeHead(Node<int>* head){
+    if(head == NULL) return head;
+
+    Node<int>* temp = head;
+    head = head->next;
+
+    delete temp;
+    return head;
+}
+
+
+int getLenList(Node<int>* head){
+    int count = 0;
+    Node<int>* mover = head;
+
+    while (mover != nullptr)
+    {
+        count++;
+        mover = mover->next;
+    }
+    return count;
+}
+
+
+void showList(Node<int>* head){
+    Node<int>* mover = head;
+
+
+    while (mover != nullptr)
+    {
+        cout<<mover->data<<" ";
+        mover = mover->next;
+    }
+    cout<<endl;
+}
+
+
+bool isPresent(Node<int>* head, int val){
+
+    Node<int>* mover = head;
+
+    while (mover != nullptr)
+    {
+        if(mover->data == val) return true;
+        mover = mover->next;
+    }
+    
+    return false;
+}
 
 int main(){
     
-    int arr[4] = {1,2,3,4};
+    vector<int> arr = {100,23,4,5,6,90};
+
+    Node<int>* head = createArray2List(arr);
+
+    showList(head);
+    int val = 10;
+    cout<<"Number of nodes: "<<Node<int>::nodeCount<<endl;
+    cout<<"Number of nodes: "<<getLenList(head)<<endl;
+
+    if(isPresent(head,val)) cout<<val<<" is PRESENT in the list"<<endl;
+    else cout<<val<<" is NOT PRESENT in the list!"<<endl;
 
 
-    Node *head = new Node(arr[0]);
+    head = removeHead(head);
+
+    cout<<head->data<<endl;
 
 
-    Node *y = head;
-
-    cout<<y<<endl;
-    cout<<head<<endl;
     return 0;
 
 }
